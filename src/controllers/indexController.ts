40a49b6a04860f91098bdd25c445cf6e2c670f5e
shakeabi <abishake.110899@gmail.com>
2020-02-08 05:12:10 +0530

    Feat: Add bottom navbar and other styles

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -3,17 +3,22 @@ import {validationResult} from "express-validator";
 import * as mail from "../handlers/mail";
 
 export const home = (req: Request, res: Response) => {
-    console.log(req.path);
-    res.render("home", {title: "Home | Hackathon Starter Kit"});
+    res.render("home", {title: "Home | CSD"});
+};
+
+export const profile = (req: Request, res: Response) => {
+    res.render("profile", {title: "Profile | CSD"});
 };
 
 export const contact = (req: Request, res: Response) => {
-    res.render("contact", {title: "Contact | Hackathon Starter Kit"});
+    res.render("contact", {title: "Contact | CSD"});
 };
+
 export const notFound = (req: Request, res: Response) => {
     console.log(req.path);
-    res.render("404", {title: "Not Found | Hackathon Starter Kit"});
+    res.render("404", {title: "Not Found | CSD"});
 };
+
 export const contactForm = (req: Request, res: Response) => {
     try {
         const errors = validationResult(req);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -24,6 +24,7 @@ function wrapAsync(fn: any) {
 
 router.get("/", indexController.home);
 router.get("/contact", indexController.contact);
+router.get("/profile", indexController.profile);
 router.get("/404", indexController.notFound);
 
 router.post(
